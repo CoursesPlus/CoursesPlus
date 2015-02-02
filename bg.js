dda4ff3283bd1587db2740e87291f88a9654be68
Alex Studer <alex_studer@outlook.com>
2015-02-01 21:23:49 -0500

    Remove unused code

--- bg.js
+++ bg.js
@@ -1,14 +1,3 @@
-/*// Called when the url of a tab changes.
-function checkForValidUrl(tabId, changeInfo, tab) {
-  // If the letter 'g' is found in the tab's URL...
-  if (tab.url.indexOf("https://courses2013.dalton.org/") > -1) {
-    // ... show the page action.
-    chrome.pageAction.show(tabId);
-  }
-};
-
-// Listen for any changes to the URL of any tab.
-chrome.tabs.onUpdated.addListener(checkForValidUrl);*/
 console.log("Courses+ background page!");
 chrome.webRequest.onBeforeSendHeaders.addListener(function(details) {
 	if (details.url.indexOf("user/icon/dalton/") > -1) {
