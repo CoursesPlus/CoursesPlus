1cb4b15a159a514af6e4fd16cff9f9482a734a48
Alex Studer <alex_studer@outlook.com>
2015-02-13 08:09:50 -0500

    Add CoursesPlusDevSync

--- browsersupport/Chrome/js/cpalloader.js
+++
@@ -1,7 +0,0 @@
-var file = "";
-if (navigator.userAgent.indexOf("Chrome") > -1) {
-	file = "chrome";
-} else if (navigator.userAgent.indexOf("Safari") > -1) {
-	file = "safari";
-}
-document.write('<script type="text/javascript" src="../js/cpal.' + file + '.js"></scr' + 'ipt>');
