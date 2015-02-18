cbd0610b9f7acdccb16564ca78e22adcbc8aba10
William <william@barkoffusa.com>
2015-02-18 08:44:34 -0500

    Added a couple of extra services
    such as Archived Courses, Schedules, and Athletics

--- js/services.js
+++ js/services.js
@@ -25,6 +25,7 @@ var services = {
 		createBlock: function() {
 			// TODO: get lunch menu from http://www.myschooldining.com/dalton/?cmd=menus
 			// This shouldn't change every day, so maybe some sort of caching?
+			// Maybe a RSS I don't know if they have one.
 		}
 	},
 	schedules: {
@@ -42,6 +43,28 @@ var services = {
 			// This shouldn't change much, so maybe some sort of caching (with a force refresh option)?
 		}
 	}
+	archivedCourses:
+		displayName: "Archived Courses",
+		description: "Allows Courses+to work on Archived Courses",
+		type: "block", // I do not know what to put here.
+		permissions: ["*://.dalton.org/"],
+		requires: [],
+		onEnable: function() {
+			// TODO: Change a bunch of links in the code. This should be simple.
+		},
+	}
+		athletecs: {
+		displayName: "Athletics",
+		description: "Displays what classes you've got next in the sidebar.",
+		type: "block",
+		permissions: ["http://www.dalton.org/program/athletics/schedules"],
+		requires: [],
+		onEnable: function() {
+			// TODO: ajax.
+		},
+		createBlock: function() {
+			// TODO: get events and games.
+		}
 };
 
 window.services = services;
