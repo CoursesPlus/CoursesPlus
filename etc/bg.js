9235fec23dd1089ee8c4a78d49f68f1b532fed94
Alex Studer <alex_studer@outlook.com>
2015-03-01 12:41:54 -0500

    Fix coursesnew with no avatar

--- etc/bg.js
+++ etc/bg.js
@@ -3,6 +3,9 @@ cpal.request.addBeforeSendHeaders(["*://courses2015.dalton.org/*"], function(det
 	if (details.url.indexOf("user/icon/dalton/") > -1) {
 		details.requestHeaders.push({name: 'X-CoursesPlus-RefererSpoof-Yay', value: "https://courses2015.dalton.org/user/profile.php"});
 	}
+	if (details.url.indexOf("theme/image.php/dalton/core") > -1 && details.url.indexOf("/u/f1") > -1) {
+		details.requestHeaders.push({name: 'X-CoursesPlus-RefererSpoof-Yay', value: "https://courses2015.dalton.org/user/profile.php"});
+	}
 	for (var i = 0; i < details.requestHeaders.length; ++i) {
 		if (details.requestHeaders[i].name === 'X-CoursesPlus-RefererSpoof-Yay') {
 			var newVal = details.requestHeaders[i].value;
