976eca5ca5d79bb5d94c9d5b7bfa514f37f9a54e
thatoddmailbox <thatoddmailbox@users.noreply.github.com>
2015-05-14 19:11:32 -0400

    Prepare for better logo handling

---
+++ js/logos.js
@@ -0,0 +1,34 @@
+var logos = {
+	regular: {
+		whitebg: "images/logos/regular.png",
+		blackbg: "images/logos/regular.png",
+		width: 328,
+		height: 80,
+	},
+	circle: {
+		whitebg: "images/logos/circle.png",
+		blackbg: "images/logos/circle.png",
+		width: 110,
+		height: 110,
+	},
+	coursesbigwordmark: {
+		whitebg: "images/logos/coursesbigwordmark.png",
+		blackbg: "images/logos/coursesbigwordmark.png",
+		width: 270,
+		height: 100,
+	},
+	coursespluswordmark: {
+		whitebg: "images/logos/whitebg/coursespluswordmark.png",
+		blackbg: "images/logos/blackbg/coursespluswordmark.png",
+		width: 194,
+		height: 31,
+	},
+	daltoncourseswordmark: {
+		whitebg: "images/logos/whitebg/daltoncourseswordmark.png",
+		blackbg: "images/logos/blackbg/daltoncourseswordmark.png",
+		width: 237,
+		height: 35,
+	}
+};
+
+window.logos = logos;
