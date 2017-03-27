42c6aeb06bcd851db4249bf927612ad3373e1c35
Alex Studer <alex_studer@outlook.com>
2017-03-27 18:34:47 +0100

    fix bug with pdfs in firefox

--- js/inject.js
+++ js/inject.js
@@ -2,11 +2,12 @@ if (window.location.href.indexOf(".pdf") != -1 || window.location.href.indexOf("
 	console.log("It's a PDF! Let's stop now.");
 } else {
 	document.addEventListener('DOMContentLoaded', onDOM_Ready, false);
-}
 
-if (cpal.extension.getBrowser() == "safari" || cpal.extension.getBrowser() == "firefox") {
-	window.services.runAll();
-	window.components.runAll();
+	if (cpal.extension.getBrowser() == "safari" || cpal.extension.getBrowser() == "firefox") {
+		window.services.runAll();
+		window.components.runAll();
+	}
+
 }
 
 function onDOM_Ready () {
